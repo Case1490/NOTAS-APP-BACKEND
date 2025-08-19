b2c804ff318dbef9e21bf46a8d347d110ee26004
Case1490 <renzoramos414@gmail.com>
2025-08-18 22:58:39 -0500

    agregamos a cors el origen permitido de nuestro front

--- src/server.js
+++ src/server.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://peppy-youtiao-4a0e2f.netlify.app/",
+    ],
   })
 );
 
